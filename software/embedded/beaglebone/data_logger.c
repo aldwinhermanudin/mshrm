4d0a6ed8be0ce5bfa36bfd30312cf112ef1940eb
aldwinakbar <aldwinakbar@gmail.com>
2016-05-27 21:17:35 +0700

    milestone: finished all requirement

--- software/embedded/beaglebone/data_logger.c
+++ software/embedded/beaglebone/data_logger.c
@@ -1,87 +1,20 @@
-#include <unistd.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/ioctl.h>
-#include <fcntl.h>
-#include <termios.h>
-#include <sys/time.h>
-#include <string.h>
-#include <mysql.h>
-#define UID_SIZE 8
-#define WRITE_SIZE 2048
-#define READ_SIZE 2048
-
-
-
-// ###################################### DECLARE MYSQL VARIABLE ################################################ //
-MYSQL 		*mysql_server;
-MYSQL_RES 	*mysql_result;
-MYSQL_ROW 	mysql_row;
-char		database_data[5][READ_SIZE];
-char 		*server;
-char 		*user;
-char 		*password;
-char		*database;
-int 		sqlConnect		(MYSQL *connection,char * server_location,char * user_admin,char * password_admin,char * database_server);
-int 		sqlQuery		(MYSQL *connection, const char *sql_query);
-int 		sqlSearch		(MYSQL *connection, MYSQL_RES *result, MYSQL_ROW row_data, char* uid, int uid_row_number);
-int 		sqlFindUID		(MYSQL *connection, MYSQL_RES *result, MYSQL_ROW row_data, const char *sql_query, char* uid, int uid_row_number);
-void 		sqlLogin		(char* server_location,char* user_data,char* user_password,char* database_name);
-void 		clearIOQueue	();
-// ###################################### DECLARE MYSQL VARIABLE ################################################ //
-
-// ###################################### DECLARE SERIAL VARIABLE ################################################ //
-
-const char*	portACM     	= "/dev/ttyACM0";
-char 		bufferRead		[READ_SIZE];
-char 		bufferWrite		[WRITE_SIZE];
-int			fd;	
-int 		serialStart		(const char* portname, speed_t baud);
-void 		serialRead		(int dataRead);
-void 		serialWrite		(const char* data_out, int data_size);
-int			serialConnect	(const char* portname, speed_t baud);
-// ###################################### DECLARE SERIAL VARIABLE ################################################ //
-
-// ###################################### DECLARE PROTOCOL VARIABLE ################################################ //
-#define PAYLOAD_SIZE 		2048
-#define STATUS_CODE_SIZE 	12
-int		decodeMessage		(char* data_input);
-char	messageStatus		[STATUS_CODE_SIZE];
-char	messagePayload		[PAYLOAD_SIZE];
-char	responseMessage		[READ_SIZE];
-// ###################################### DECLARE PROTOCOL VARIABLE ################################################ //
-
-// ID Swapper
-
-void swap(int *a, int *b);
-int segregate (int arr[], int size);
-int findMissingPositive(int arr[], int size);
-int findMissing(int arr[], int size);
-
-// ID Swapper
-
-
-
+#include "include/mshrm_mysql.h"
+#include "include/mshrm_communication.h"
+// TODO : Not sure if need a checking on when querying to remote server
 int main(){
 	
 	serialConnect(portACM, B9600);
-	sqlLogin("localhost","root","hermanudin","mshrm_client");
-	mysql_server = mysql_init(NULL);
-	sqlConnect(mysql_server, server, user, password, database);
+	setSQLConfiguration();
+	char main_query[MAX_QUERY_BYTE];
 	
-	// need to add checking function if serialConnect and sqlConnect a success
-	
-	const char *uid_sql_query = "SELECT * FROM employee_uid";
-	char insertUID[256];
-
 	while(1){
-		clearIOQueue();
+		clearIOQueue();	
 		serialRead(READ_SIZE);
 		printf("\nWe received\t: %s\n",bufferRead);
 		
 		if (decodeMessage(bufferRead) != 0){
-			sprintf(responseMessage, ":02,Incorrect message format;");
-			serialWrite(responseMessage,strlen(responseMessage));
+			serialWriteErrorMessage("Incorrect message format");
 			printf("Incorrect message format\n");
 			continue;
 		} 
@@ -91,349 +24,142 @@ int main(){
 			printf("Message Payload\t: %s\n\n",messagePayload);
 			
 			if (strcmp(messageStatus, "00") == 0){
-				int found = sqlFindUID(mysql_server,mysql_result,mysql_row,uid_sql_query,messagePayload,0);
+				printf ("Looking for the '.' character in \"%s\"...\n",messagePayload);
+				char decoded_payload_messages[7][32];
+				int dot_location[6];
+				int valid_format = 0;
+				int i = 0;
+				char * pch;
+				pch=strchr(messagePayload,'.');
+				while (pch!=NULL){
+					printf ("found at %ld\n",(pch-messagePayload));
+					dot_location[i] = (pch-messagePayload);
+					pch=strchr(pch+1,'.');
+					i++;
+					valid_format++;
+				}
+				
+				if (valid_format != 6){
+					serialWriteErrorMessage("Incorrect payload format");
+					printf("Incorrect payload format\n");
+					continue;
+				}
+									
+				strncpy(decoded_payload_messages[0], &messagePayload[0], dot_location[0]);
+				decoded_payload_messages[0][dot_location[0]] = '\0';
+				for(i=1;i <= 5; i++){
+					strncpy(decoded_payload_messages[i], &messagePayload[dot_location[i-1]+1],dot_location[i]-dot_location[i-1]-1);
+					decoded_payload_messages[i][dot_location[i]-dot_location[i-1]-1] = '\0';
+				}
+				strncpy(decoded_payload_messages[6], &messagePayload[dot_location[5]+1], 2);
+				decoded_payload_messages[6][2] = '\0';
+				
+				sprintf(main_query, "SELECT * FROM employee_uid where flash_id='%s'",decoded_payload_messages[0]);
+				int found = sqlQueryCheck(local_main_connection,main_query);
 				if (found){
+					sqlFetchDataToArray(local_main_connection,main_query,1,5);
 					printf("We found something\n");
-					printf("Belongs to %s\n",database_data[1]);
+					printf("Belongs to %s\n",sql_fetched_data[0][1]);
 					
 					// Upload log to database
-					printf("Uploading to Local Database\n");
-					sprintf(insertUID,"INSERT INTO work_log (uid) VALUES ('%s')",database_data[0]);
-					sqlQuery(mysql_server,insertUID);
+					char local_timestamp[32];
+					sprintf(local_timestamp, "%s-%s-%s %s:%s:%s",decoded_payload_messages[3],decoded_payload_messages[2],decoded_payload_messages[1],decoded_payload_messages[4],decoded_payload_messages[5],decoded_payload_messages[6]);
+					sprintf(main_query,"INSERT INTO work_log (uid,timestamp) VALUES ('%s','%s')",sql_fetched_data[0][0],local_timestamp);
+					printf("Uploading %s and %s to Local Database\n", sql_fetched_data[0][0],local_timestamp);
+					sqlQuery(local_main_connection,main_query);
 					
 					// Sending OK message to serial port
 					printf("Sending OK message\n");
-					sprintf(responseMessage, ":01,%s;",database_data[2]);
-					serialWrite(responseMessage,strlen(responseMessage));
+					serialWriteSuccessMessage(sql_fetched_data[0][2]);
 				}
 				
 				else if (!found){
-					sprintf(responseMessage, ":02,ID Not Found;");
-					serialWrite(responseMessage,strlen(responseMessage));
+					serialWriteErrorMessage(":02,ID Not Found;");
 					printf("We did not found something\n");
 					// send to serial error
 				}				
 			}
 			
 			else if (strcmp(messageStatus, "01") == 0){
-				printf("\n01 Request Message\n");
-			}
-			
-			else if (strcmp(messageStatus, "02") == 0){
-				printf("\n02 Request Message\n");
+				sprintf(main_query, "SELECT * from device_information where configuration='admin_password'");
+				int found = sqlQueryCheck(local_main_connection, main_query);
+				if (found){
+					sqlFetchDataToArray(local_main_connection, main_query , 1, 3);
+					
+					// Sending OK message to serial port
+					printf("Sending OK message\n");
+					serialWriteSuccessMessage(sql_fetched_data[0][2]);
+				}
+				
+				else {
+					serialWriteErrorMessage(":02,Not Found;");
+					printf("We did not found something\n");
+					// send to serial error
+				}				
 			}
 			
 			else if (strcmp(messageStatus, "03") == 0){
-				printf("\n03 Request Message\n");
+				
+				sprintf(main_query, "SELECT * from employee_uid");
+				int found = sqlQueryCheck(local_main_connection, main_query);
+				if (found){
+					int flash_id_total = sqlFetchDataToArray(local_main_connection, main_query , MAX_EMPLOYEE, 5);
+					int i = 0;
+					int flash_id[MAX_EMPLOYEE];
+					for(i = 0; i < flash_id_total; i++){
+						flash_id[i] = atoi(sql_fetched_data[i][3]);
+					}
+					int available_flash_id = findMissing(flash_id, flash_id_total);
+					char available_flash_id_in_char[8];
+					printf("Available Flash ID is %d ", available_flash_id);	
+					// Sending OK message to serial port
+					printf("Sending OK message\n");
+					sprintf(available_flash_id_in_char,"%d",available_flash_id);
+					serialWriteSuccessMessage(available_flash_id_in_char);
+				}
+				
+				else {
+					serialWriteErrorMessage(":02,Not Found;");
+					printf("We did not found something\n");
+					// send to serial error
+				}
 			}
 			
+			else if (strcmp(messageStatus, "04") == 0){
+				printf ("Looking for the '.' character in \"%s\"...\n",messagePayload);
+				char decoded_payload_messages[2][32];
+				int dot_location;
+				char * pch;
+				pch=strchr(messagePayload,'.');
+				if (pch!=NULL){
+					printf ("found at %ld\n",(pch-messagePayload));
+					dot_location = (pch-messagePayload);
+				}
+				
+				else {
+					serialWriteErrorMessage("Incorrect payload format");
+					printf("Incorrect payload format\n");
+					continue;
+				}
+									
+				strncpy(decoded_payload_messages[0], messagePayload, dot_location);
+				decoded_payload_messages[0][dot_location] = '\0';
+				strcpy(decoded_payload_messages[1], &messagePayload[dot_location+1]);
+				
+				sprintf(main_query,"INSERT INTO employee_uid (flash_id, uid) VALUES ('%s','%s')",decoded_payload_messages[0],decoded_payload_messages[1]);
+				printf("Inserting data : %s %s\n", decoded_payload_messages[0],decoded_payload_messages[1]);
+				sqlQuery(local_main_connection,main_query);
+				serialWriteSuccessMessage("OK");
+			}
+			
+			else{
+				serialWriteErrorMessage("Unknown Code");
+				printf("Unknown Code\n");
+				continue;
+			}
 		}
 	}
 	close(fd);
-	mysql_close(mysql_server);
+	mysql_close(local_main_connection);
+	mysql_close(server_main_connection);
 }
-
-void clearIOQueue(){
-	tcflush(fd, TCIOFLUSH);
-	usleep(50000);
-}
-int serialStart(const char* portname, speed_t baud){
-	// Open the serial port as read/write, not as controlling terminal, and
-	// don't block the CPU if it takes too long to open the port.
-	fd = open(portname, O_RDWR | O_NOCTTY);
-		
-	if (fd == -1) {
-      return 0;
-	}
-	
-	/* wait for the Arduino to reboot */
-	usleep(3500000);
-	
-	struct termios toptions;	// struct to hold the port settings
-	
-	// Fetch the current port settings
-	tcgetattr(fd, &toptions);
-	
-	// Set Input and Output BaudRate
-	cfsetispeed(&toptions, baud);
-	cfsetospeed(&toptions, baud);
-	
-	// Set up the frame information.
-	toptions.c_cflag &= ~PARENB;	// no parity
-	toptions.c_cflag &= ~CSTOPB;	// one stop bit
-	toptions.c_cflag &= ~CSIZE;		// clear frame size info
-	toptions.c_cflag |= CS8;		// 8 bit frames
-	
-	// c_cflag contains a few important things- CLOCAL and CREAD, to prevent
-	// this program from "owning" the port and to enable receipt of data.
-	// Also, it holds the settings for number of data bits, parity, stop bits,
-	// and hardware flow control.
-	toptions.c_cflag |= CREAD ;
-	toptions.c_cflag |= CLOCAL;
-	
-	/* no hardware flow control */
-	toptions.c_cflag &= ~CRTSCTS;
-	 
-	/* disable input/output flow control, disable restart chars */
-	toptions.c_iflag &= ~(IXON | IXOFF | IXANY);
-	 
-	/*
-	ICRNL
-	: map CR to NL (otherwise a CR input on the other computer
-	will not terminate input)
-	otherwise make device raw (no other input processing)
-	*/
-	toptions.c_iflag = ICRNL;
-	 
-	 /* disable canonical input, disable echo,
-	 disable visually erase chars,
-	 disable terminal-generated signals */
-	 toptions.c_lflag = ICANON;
-	 /* disable output processing */
-	 toptions.c_oflag =  ~OPOST;
-	 
-	// Now that we've populated our options structure, let's push it back to the
-	//   system.
-	tcsetattr(fd, TCSANOW, &toptions);
-	
-	usleep(1000000);
-	
-	// Flush the input and output buffer one more time.
-	tcflush(fd, TCIOFLUSH);
-	return 1;
-}
-
-void serialRead(int dataRead){
-	// Now, let's wait for an input from the serial port.
-	clearIOQueue();
-	fcntl(fd, F_SETFL, 0); // block until data comes in
-	read(fd, bufferRead, dataRead);
-	char* nl_remover = strchr(bufferRead,'\n');
-	bufferRead[nl_remover-bufferRead] = '\0';
-}
-
-void serialWrite(const char* data_out, int data_size){
-	clearIOQueue();
-	sprintf(bufferWrite,"%s",data_out);
-	int n = write(fd, bufferWrite, data_size);
-	if (n < 0)
-		fputs("write() failed!\n", stderr);
-}
-
-int serialConnect(const char* portname, speed_t baud){
-	
-	int serial_open_status = serialStart(portACM, B9600);
-	
-	if (serial_open_status){
-		printf("\nArduino connection successful\n");
-		return 0;
-	}
-	else {
-		
-		printf("\nArduino connection failed\n");
-		return -1;
-	}
-}
-
-int sqlConnect(MYSQL *connection,char * server_location,char * user_admin,char * password_admin,char * database_server){
-	
-	/* Connect to database */
-	if (!mysql_real_connect(connection, server_location, user_admin, password_admin, database_server, 0, NULL, 0)) {
-		fprintf(stderr, "%s\n", mysql_error(connection));
-		return(-1);
-	}
-	
-	else {
-		printf("Database Connection Successful\n");
-		return(0);
-	}
-}
-
-int sqlQuery(MYSQL *connection, const char *sql_query){
-	
-	if (mysql_query(connection, sql_query)) {
-		fprintf(stderr, "%s\n", mysql_error(connection));
-		return -1;
-	}
-	
-	else {
-		return 0;
-	}
-}
-
-int sqlSearch( MYSQL *connection, MYSQL_RES *result, MYSQL_ROW row_data, char* uid, int uid_row_number){
-	int found_state = 0;
-	result = mysql_use_result(connection);
-	
-	while ((row_data = mysql_fetch_row(result)) != NULL){
-		
-		if (strcmp(uid,row_data[uid_row_number]) == 0){
-			//printf("%s \n", row_data[1]);
-			strcpy(database_data[0], row_data[0]);
-			strcpy(database_data[1], row_data[1]);
-			strcpy(database_data[2], row_data[2]);
-			strcpy(database_data[3], row_data[3]);		
-			found_state = 1;
-		}
-	}
-	
-	mysql_free_result(result);
-	return found_state;
-	
-}
-
-int sqlFindUID( MYSQL *connection, MYSQL_RES *result, MYSQL_ROW row_data, const char *sql_query, char* uid, int uid_row_number){
-	sqlQuery(connection,sql_query);
-	return sqlSearch(connection,result,row_data,uid,uid_row_number);
-}
-
-void sqlLogin(char* server_location,char* user_data,char* user_password,char* database_name){
-	
-	server = server_location;
-	user = user_data;
-	password = user_password;
-	database = database_name;
-}
-
-int decodeMessage(char* data_input){
-	// consider adding this to clear message
-	memset(messageStatus,'\0',STATUS_CODE_SIZE);
-    memset(messagePayload,'\0',PAYLOAD_SIZE);
-	
-	char * pointer_to_status_code = strchr(data_input,':');
-	if (pointer_to_status_code == NULL)
-		return -1;	
-	else 
-		pointer_to_status_code += 1;
-	strncpy(messageStatus, pointer_to_status_code, 2);
-	
-	char * pointer_to_payload = strchr(data_input,',');
-	if (pointer_to_payload == NULL)
-		return -2;
-	else
-		pointer_to_payload += 1;
-	strcpy(messagePayload, pointer_to_payload);	
-	
-	//also consider this to copy payload data
-	int end_of_message_location = strchr(messagePayload,';') - messagePayload;
-	//int end_of_message_location = strlen(messagePayload) - 1;
-	messagePayload[end_of_message_location] = '\0';
-	return 0;
-}
-
-/* Utility to swap to integers */
-void swap(int *a, int *b)
-{
-    int temp;
-    temp = *a;
-    *a   = *b;
-    *b   = temp;
-}
- 
-/* Utility function that puts all non-positive (0 and negative) numbers on left 
-  side of arr[] and return count of such numbers */
-int segregate (int arr[], int size)
-{
-    int j = 0, i;
-    for(i = 0; i < size; i++)
-    {
-       if (arr[i] <= 0)  
-       {
-           swap(&arr[i], &arr[j]);
-           j++;  // increment count of non-positive integers
-       }
-    }
- 
-    return j;
-}
- 
-/* Find the smallest positive missing number in an array that contains
-  all positive integers */
-int findMissingPositive(int arr[], int size)
-{
-  int i;
- 
-  // Mark arr[i] as visited by making arr[arr[i] - 1] negative. Note that 
-  // 1 is subtracted because index start from 0 and positive numbers start from 1
-  for(i = 0; i < size; i++)
-  {
-    if(abs(arr[i]) - 1 < size && arr[abs(arr[i]) - 1] > 0)
-      arr[abs(arr[i]) - 1] = -arr[abs(arr[i]) - 1];
-  }
- 
-  // Return the first index value at which is positive
-  for(i = 0; i < size; i++)
-    if (arr[i] > 0)
-      return i+1;  // 1 is added becuase indexes start from 0
- 
-  return size+1;
-}
- 
-/* Find the smallest positive missing number in an array that contains
-  both positive and negative integers */
-int findMissing(int arr[], int size)
-{
-   // First separate positive and negative numbers
-   int shift = segregate (arr, size);
- 
-   // Shift the array and call findMissingPositive for
-   // positive part
-   return findMissingPositive(arr+shift, size-shift);
-}
-/*
-* Arduino Example
-
-void setup() {
-	// initialize both serial ports:
-	Serial.begin(9600);
-	pinMode(A0, INPUT);
-}
-
-void loop() {
-	// read from port 1, send to port 0:
-	if (Serial.available()) {
-		String inByte = Serial.readString();
-		delay(3000);
-		Serial.print("2a22e8d5");
-		
-	}
-	
-	if(digitalRead(A0)){
-		Serial.print("2a12e8d5");
-		delay(5000);
-	}
-	
-}
-
-// Arduino Example 2 //
-void setup() {
-	// initialize both serial ports:
-	Serial.begin(9600);
-	pinMode(A0, INPUT);
-}
-
-void loop() {
-	// read from port 1, send to port 0:
-	if (Serial.available()) {
-	  String inByte = Serial.readString();
-          if(inByte[2] == '1'){
-            Serial.println(inByte);
-          }
-          else if(inByte[2] == '2'){
-            delay(2000);
-          }
-          while (Serial.available()) {
-            Serial.read();
-          }
-          Serial.flush();
-                
-	}
-	
-	if(digitalRead(A0)){
-		Serial.println(":00,100;");
-		delay(50);
-	}
-	
-}
-
-*/
--- software/embedded/beaglebone/worklog_synchronizer.c
+++ software/embedded/beaglebone/worklog_synchronizer.c
@@ -1,53 +1,13 @@
-#include <mysql.h>
-#include <stdio.h>
-#define UID_SIZE 8
+#include "include/mshrm_mysql.h"
+
 #define UID_COLUMN 1
 #define TIMESTAMP_COLUMN 2
-#define MAX_QUERY_BYTE 512
-/* table column
- * 	0 	 1	 	   2
- * no	uid	   timestamp 
- * 
- */
- 
- 
-MYSQL *server_main_connection;
-char *server_ip;
-char *server_user;
-char *server_password;
-char *server_database;
-
-MYSQL *local_main_connection;
-char *local_ip;
-char *local_user;
-char *local_password;
-char *local_database;
-
-char row_local_result[3][256];
-
-int sqlConnect(MYSQL *connection,char * server_location,char * user_admin,char * password_admin,char * database_server);
-int sqlQuery(MYSQL *connection, const char *sql_query);
-int sqlSearch( MYSQL *connection, MYSQL_RES *result, MYSQL_ROW row_data, char* uid, int uid_row_number);
 int sqlDataUpload(MYSQL *local_connection, MYSQL *server_connection, const char *sql_query);
 int sqlDataUploadCheck( MYSQL *local_connection, MYSQL *server_connection, const char *sql_query);
-int sqlFindUID( MYSQL *connection, MYSQL_RES *result, MYSQL_ROW row_data, const char *sql_query, char* uid, int uid_row_number);
-void sqlServerLogin(char* server_location,char* server_user_data,char* server_user_password,char* server_database_name);
-void sqlLocalLogin(char* local_location,char* local_user_data,char* local_user_password,char* local_database_name);
-int sqlAutoReconnect(MYSQL *connection, int reconnect_status);
-int sqlQueryCheck( MYSQL *connection, const char *sql_query);
 
 int main() {
 	
-	sqlServerLogin("localhost","root","hermanudin","mshrm_server");
-	sqlLocalLogin("localhost","root","hermanudin","mshrm_client");
-	server_main_connection = mysql_init(NULL);
-	local_main_connection = mysql_init(NULL);
-	
-	sqlAutoReconnect(server_main_connection,1);
-	sqlAutoReconnect(local_main_connection,1);
-	
-	sqlConnect(server_main_connection, server_ip, server_user, server_password, server_database);
-	sqlConnect(local_main_connection, local_ip, local_user, local_password, local_database);
+	setSQLConfiguration();
 	
 	char upload_query[MAX_QUERY_BYTE] = "SELECT * FROM work_log LIMIT 1";			
 	char upload_check_query[MAX_QUERY_BYTE];
@@ -59,13 +19,13 @@ int main() {
 			int upload_status = sqlDataUpload(local_main_connection, server_main_connection,upload_query);
 			
 			// check if data is uploaded, then delete the data in the local server
-			sprintf(upload_check_query,"SELECT * FROM work_log where uid='%s' AND timestamp='%s'",row_local_result[UID_COLUMN],row_local_result[TIMESTAMP_COLUMN]);
+			sprintf(upload_check_query,"SELECT * FROM work_log where uid='%s' AND timestamp='%s'",sql_fetched_data[0][UID_COLUMN],sql_fetched_data[0][TIMESTAMP_COLUMN]);
 			int upload_check = sqlDataUploadCheck(local_main_connection, server_main_connection,upload_check_query);
 			
 			// check for successful upload
 			if (upload_status && upload_check){
 				
-				printf("%s %s",row_local_result[UID_COLUMN],row_local_result[TIMESTAMP_COLUMN]);
+				printf("%s %s",sql_fetched_data[0][UID_COLUMN],sql_fetched_data[0][TIMESTAMP_COLUMN]);
 				printf("\nUpload Success");
 				printf("\nWait 0.01s.\n");
 				//usleep(10000);
@@ -91,69 +51,6 @@ int main() {
 	mysql_close(local_main_connection);
 }
 
-
-
-int sqlConnect(MYSQL *connection,char * server_location,char * user_admin,char * password_admin,char * database_server){
-	
-	/* Connect to database */
-	if (!mysql_real_connect(connection, server_location, user_admin, password_admin, database_server, 0, NULL, 0)) {
-		fprintf(stderr, "%s\n", mysql_error(connection));
-		return(-1);
-	}
-	
-	else {
-		printf("Connection Successful\n");
-		return(0);
-	}	
-}
-
-int sqlQuery(MYSQL *connection, const char *sql_query){
-	
-	/* send SQL query */
-	if (mysql_query(connection, sql_query)) {
-		fprintf(stderr, "%s\n", mysql_error(connection));
-		return -1;
-	}
-	
-	else {
-		
-		return 0;
-	}
-}
-
-int sqlSearch( MYSQL *connection, MYSQL_RES *result, MYSQL_ROW row_data, char* uid, int uid_row_number){
-	int found_state = 0;
-	result = mysql_use_result(connection);
-	/* output table name */
-	while ((row_data = mysql_fetch_row(result)) != NULL){
-		
-		if (strcmp(uid,row_data[uid_row_number]) == 0){
-			//printf("%s \n", row_data[1]);
-			found_state = 1;
-		}
-	}
-	
-	mysql_free_result(result);
-	return found_state;
-	
-}
-
-int sqlQueryCheck( MYSQL *connection, const char *sql_query){
-	MYSQL_RES *result; 
-	MYSQL_ROW row_data;
-	sqlQuery(connection, sql_query);
-	int found_state = 0;
-	result = mysql_use_result(connection);
-	/* output table name */
-	if ((row_data = mysql_fetch_row(result)) != NULL){
-			found_state = 1;
-	}
-	
-	mysql_free_result(result);
-	return found_state;
-	
-}
-
 // missing code to re-check if upload success
 int sqlDataUpload( MYSQL *local_connection, MYSQL *server_connection, const char *sql_query){
 	
@@ -168,8 +65,8 @@ int sqlDataUpload( MYSQL *local_connection, MYSQL *server_connection, const char
 	if((row_data = mysql_fetch_row(result)) != NULL){
 		
 		// SQL query to insert data to main server
-		sprintf(row_local_result[UID_COLUMN],"%s",row_data[UID_COLUMN]);
-		sprintf(row_local_result[TIMESTAMP_COLUMN],"%s",row_data[TIMESTAMP_COLUMN]);
+		sprintf(sql_fetched_data[0][UID_COLUMN],"%s",row_data[UID_COLUMN]);
+		sprintf(sql_fetched_data[0][TIMESTAMP_COLUMN],"%s",row_data[TIMESTAMP_COLUMN]);
 		
 		// SQL query to insert data to main server
 		sprintf(insert_data_query,"INSERT INTO work_log (uid,timestamp) VALUES ('%s','%s')",row_data[UID_COLUMN], row_data[TIMESTAMP_COLUMN]);
@@ -204,31 +101,4 @@ int sqlDataUploadCheck( MYSQL *local_connection, MYSQL *server_connection, const
 	}
 	mysql_free_result(result);
 	return found_state;
-}
-
-int sqlFindUID( MYSQL *connection, MYSQL_RES *result, MYSQL_ROW row_data, const char *sql_query, char* uid, int uid_row_number){
-	sqlQuery(connection,sql_query);
-	return sqlSearch(connection,result,row_data,uid,uid_row_number);	
-}
-
-void sqlServerLogin(char* server_location,char* server_user_data,char* server_user_password,char* server_database_name){
-	
-server_ip		=	server_location;
-server_user		=	server_user_data;
-server_password	=	server_user_password;
-server_database	=	server_database_name;
-}
-
-void sqlLocalLogin(char* local_location,char* local_user_data,char* local_user_password,char* local_database_name){
-	
-local_ip		=	local_location;
-local_user		=	local_user_data;
-local_password	=	local_user_password;
-local_database	=	local_database_name;
-}
-
-int sqlAutoReconnect(MYSQL *connection, int reconnect_status){
-	
-	my_bool reconnect = reconnect_status;
-	return mysql_options(connection, MYSQL_OPT_RECONNECT, &reconnect);
 }
